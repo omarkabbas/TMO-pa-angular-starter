9036b422d1983cbb78aa3f3381823355dc7c71cf
Omar Abbas <Omark.abbas@yahoo.com>
2022-01-07 15:29:15 -0600

    adding stories 2 and 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pa-starter-angular';
+  show = false;
+
+  recipeName: string = '';
+  recipeInstructions: string = '';
+  recipe_Name: string = '';
+  
+
+  onClick() {
+    this.show = !this.show;
+  }
+  addRecipe() {
+    
+    console.log(this.recipeName);
+    console.log(this.recipeInstructions);
+    this.recipe_Name = this.recipeName;
+    
+  }
 }

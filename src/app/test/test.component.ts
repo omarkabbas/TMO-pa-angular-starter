27e3f75dc5f6e392f5f7aecb8be9353f5e32b9c1
Omar Abbas <Omark.abbas@yahoo.com>
2022-01-07 19:31:15 -0600

    try1

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
   show = false;
   recipe_Name: any = '';
-
+  @ViewChild("recipeName") recipeName:any;
   //constructor() { }
 
   ngOnInit() {
@@ -16,7 +16,8 @@ export class TestComponent implements OnInit {
   onClickSubmit(recipeForm:any) {
     console.log("You have entered : " , recipeForm.recipeName); 
     console.log("You have entered : " , recipeForm.recipeInstructions);
-    this.recipe_Name = recipeForm.recipeName;
+    //this.recipe_Name = recipeForm.recipeName;
+    this.recipe_Name = this.recipeName.nativeElement.value;
  }
  onClick() {
   this.show = !this.show;

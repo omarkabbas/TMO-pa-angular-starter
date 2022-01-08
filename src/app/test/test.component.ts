ea4b9919722c6fc503b4b818007804b8bfbdf9a7
Omar Abbas <Omark.abbas@yahoo.com>
2022-01-07 20:04:39 -0600

    finally working

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,13 +9,16 @@ export class TestComponent implements OnInit {
   show = false;
   recipe_Name: any = '';
   @ViewChild("recipeName") recipeName:any;
+  @ViewChild("recipeInstructions") recipeInstructions:any;
   //constructor() { }
 
   ngOnInit() {
   }
   onClickSubmit(recipeForm:any) {
-    console.log("You have entered : " , recipeForm.recipeName); 
-    console.log("You have entered : " , recipeForm.recipeInstructions);
+    //console.log("You have entered : " , recipeForm.recipeName); 
+    //console.log("You have entered : " , recipeForm.recipeInstructions);
+    console.log("You have entered : " , this.recipeName.nativeElement.value); 
+    console.log("You have entered : " , this.recipeInstructions.nativeElement.value); 
     //this.recipe_Name = recipeForm.recipeName;
     this.recipe_Name = this.recipeName.nativeElement.value;
  }
